d5a445cc8526141906704ef8d307ff5b3ab356d8
Hideaki Kawai <kayhide@gmail.com>
2020-12-15 12:04:34 +0900

    Create utils

---
+++ utils.erl
@@ -0,0 +1,18 @@
+-module(utils).
+-compile(export_all).
+
+fst({ F, _ }) -> F.
+snd({ _, S }) -> S.
+
+bool(F, _T, false) -> F;
+bool(_F, T, true) -> T.
+
+chunks(_N, []) -> [];
+chunks(N, Xs) ->
+  case lists:split(N, Xs) of
+    { Chunk, Ys } -> [Chunk | chunks(N, Ys)]
+  end.
+
+puts() -> io:format("~n").
+puts(X) when is_list(X) -> io:format("~s~n", [X]);
+puts(X) -> io:format("~p~n", [X]).
