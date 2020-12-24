0405ba73b4061a7002e453e557df3fa234b30892
Hideaki Kawai <kayhide@gmail.com>
2020-12-24 11:33:40 +0900

    Solve day23 part 2

--- day23.erl
+++ day23.erl
@@ -11,13 +11,17 @@ run() ->
 
 run1(Input) ->
   Ns = construct(Input),
-  Ns100 = iterate(100, fun move/1, Ns),
-  { Xs, [1 | Ys] } = lists:splitwith(fun(I) -> I /= 1 end, Ns100),
-  Ans = lists:concat([ integer_to_list(I) || I <- Ys ++ Xs ]),
+  iterate(100, fun move/1, Ns),
+  [_ | Xs] = take(9, 1, Ns),
+  Ans = lists:concat([ integer_to_list(I) || I <- Xs ]),
   echo(Ans).
 
 run2(Input) ->
-  Ans = 2,
+  Ns = construct(1000 * 1000, Input),
+  iterate(10 * 1000 * 1000, fun move/1, Ns),
+  [_, X, Y | _] = take(3, 1, Ns),
+  puts([X, Y]),
+  Ans = X * Y,
   puts(Ans).
 
 
@@ -25,27 +29,64 @@ construct(Input) -> construct(nil, Input).
 
 construct(Max, Input) ->
   Ns = [ list_to_integer([C]) || Line <- Input, C <- Line ],
-  Ns1 =
+  { Ns1, Max1 } =
     case Max of
-      nil -> Ns;
-      _ -> Ns ++ lists:seq(lists:max(Ns), Max)
+      nil -> { Ns, lists:max(Ns) };
+      _ -> { Ns ++ lists:seq(lists:max(Ns) + 1, Max), Max }
     end,
-  Ns1.
+  G = digraph:new(),
+  Vs = [ digraph:add_vertex(G, N) || N <- Ns1 ],
+  [ digraph:add_edge(G, V1, V2) || { V1, V2 } <- lists:zip(Ns1, tl(Ns1) ++ [hd(Ns1)]) ],
+  { G, hd(Vs), Max1 }.
   
-move([C, N1, N2, N3 | Ns]) ->
-  C1 = find_next(C - 1, lists:max(Ns), [N1, N2, N3]),
-  { Pre, [C1 | Post] } = lists:splitwith(fun(I) -> I /= C1 end, Ns),
-  Pre ++ [C1, N1, N2, N3] ++ Post ++ [C].
+move({ G, Cur, Max }) ->
+  [N1] = digraph:out_neighbours(G, Cur),
+  [N2] = digraph:out_neighbours(G, N1),
+  [N3] = digraph:out_neighbours(G, N2),
+  [N4] = digraph:out_neighbours(G, N3),
+  Dst = find_destination(Cur - 1, Max, [N1, N2, N3]),
+  % puts(take(9, Cur, { G, Cur, Max })),
+  % puts({ current, Cur, destination, Dst, taking, [N1, N2, N3] }),
+  outsert(G, N1, N3),
+  insert_after(G, Dst, N1, N3),
+  { G, N4, Max }.
+
+outsert(G, V0, V1) ->
+  [Prev] = digraph:in_neighbours(G, V0),
+  [Next] = digraph:out_neighbours(G, V1),
+  digraph:del_edges(G, digraph:in_edges(G, V0)),
+  digraph:del_edges(G, digraph:out_edges(G, V1)),
+  digraph:add_edge(G, Prev, Next).
+
+insert_after(G, Prev, V0, V1) ->
+  [Next] = digraph:out_neighbours(G, Prev),
+  digraph:del_edges(G, digraph:out_edges(G, Prev)),
+  digraph:del_edges(G, digraph:in_edges(G, Next)),
+  digraph:add_edge(G, Prev, V0),
+  digraph:add_edge(G, V1, Next).
+  
+take(Count, At, { G, _, _ }) -> take(Count, At, G);
+take(Count, At, G) ->
+  { _, Ns } =
+  lists:foldl(
+    fun(_, { V, Vs }) ->
+      [Next] = digraph:out_neighbours(G, V) ,
+      { Next, Vs ++ [V] }
+    end, { At, [] }, lists:seq(1, Count)),
+  Ns.
   
 
-find_next(0, Max, Out) -> find_next(Max, Max, Out);
-find_next(N, Max, Out) ->
+find_destination(0, Max, Out) -> find_destination(Max, Max, Out);
+find_destination(N, Max, Out) ->
   case lists:member(N, Out) of
     false -> N;
-    true -> find_next(N - 1, Max, Out)
+    true -> find_destination(N - 1, Max, Out)
   end.
 
 iterate(0, _, X) -> X;
 iterate(N, F, X) ->
+  case N rem 100000 of
+    0 -> puts(N);
+    _ -> nop
+  end,
   iterate(N - 1, F, F(X)).
-  
